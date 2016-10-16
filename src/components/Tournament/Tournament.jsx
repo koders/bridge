c98f938a46563a51dfcc0c3b8d2f483bb864ce4d
Rihards Fridrihsons <rfridrihsons@gmail.com>
2016-10-16 19:47:47 +0300

    Add tabs to tournament view

--- src/components/Tournament/Tournament.jsx
+++ src/components/Tournament/Tournament.jsx
@@ -10,6 +10,15 @@ const styles = reactCSS({
   'default': {
     collection: {
       border: 0
+    },
+    tabContainer: {
+      marginLeft: 0
+    },
+    tabBody: {
+      padding: '20px'
+    },
+    tab: {
+      position: 'relative'
     }
   },
 })
@@ -19,6 +28,10 @@ export default class Tournament extends Component {
 
   componentWillMount() {
     this.props.tournamentStore.readTournament(this.props.params.tournamentId);
+  }
+
+  componentDidMount() {
+    $('ul.tabs').tabs();
   }
 
   render() {
@@ -32,10 +45,33 @@ export default class Tournament extends Component {
     return (
       <div className="tournament">
         <h3 className="header center-align w300">{tournament.name}</h3>
-        <h4 className="header center-align w300">Participants</h4>
-        <div className="collection" style={styles.collection}>
-          { participants }
+        <div className="row content-center">
+          <div className="col s12 m8 l4" style={styles.tabContainer}>
+
+        <ul className="tabs">
+          <li className="tab col s3" style={styles.tab}>
+            <a href="#participants" className="blue-grey-text text-darken-4">
+              Participants
+              <span className="badge">
+                {tournament.participants ? tournament.participants.length : 0}
+              </span>
+            </a>
+          </li>
+          <li className="tab col s3" style={styles.tab}>
+            <a href="#map" className="blue-grey-text text-darken-4">
+              Show in map
+            </a>
+          </li>
+         </ul>
+         </div></div>
+        <div style={styles.tabBody} id="participants" className="col s12">
+          <div className="collection" style={styles.collection}>
+            {participants}
+          </div>
         </div>
+        <div style={styles.tabBody} id="map" className="col s12">...map...</div>
+
+
       </div>
     )
   }
